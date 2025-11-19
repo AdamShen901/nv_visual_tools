627f557b3ba823e25100ce37f35ce8b1b9017dc8
YifanShen <44206009+AdamShen901@users.noreply.github.com>
2025-11-19 15:49:06 +0800

    修正相对路径的表达方式

--- streamlit_app_trading_nv.py
+++ streamlit_app_trading_nv.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-default_folder_path = r'\source'
+default_folder_path = r'./source'
 
 
 def visualize_backtest_results(folder_path, chart_height=6, mdd_alpha=0.3):
@@ -489,3 +489,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
